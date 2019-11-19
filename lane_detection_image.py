bafaa8885c67c921e5589dd26b9a49eb6c36e959
Shivam Chourey <34999769+ShivamChourey@users.noreply.github.com>
2019-11-19 07:50:39 -0500

    Comments and updates

--- lane_detection_image.py
+++ lane_detection_image.py
@@ -1,7 +1,13 @@
+# Shivam Chourey
+# Lane detection in an images
+# I learned this technique following tutorial by Rayan Smiles and PrgrammingKnowledge
+
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Function to get the coordinates where lines must be drawn
+# The lines are given by the argument line_parameters
 def make_coord(image, line_parameters):
     slope, intercept = line_parameters
     y1 = image.shape[0]
@@ -10,6 +16,8 @@ def make_coord(image, line_parameters):
     x2 = int((y2 - intercept)/slope)
     return np.array([x1, y1, x2, y2])
 
+# This function takes in a set of lines, identifies the left and right lines based on slope
+# Averages the left set and right set of lines to return a single left and a right line
 def average_lines(image, lines):
     left = []
     right = []
@@ -28,12 +36,15 @@ def average_lines(image, lines):
     right_line = make_coord(image, right_avg)
     return np.array([left_line, right_line])
 
-
+# This function takes in an image, converts it to grayscale and performs Canny edge detection
 def Canny(im):
     gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
     canny = cv2.Canny(gray, 100, 200)
     return canny
 
+# This function defines a region of interest in the image where we want to identify lanes
+# Right now, this has hard-coded values specific to the image and videos used
+# I plan to make it generic in near future
 def region_of_interest(im):
     height = im.shape[0]
     polygons = np.array([
@@ -44,6 +55,7 @@ def region_of_interest(im):
     masked_lane = np.bitwise_and(im, mask)
     return masked_lane
 
+# Function  to display lines on an image, both passed as argument
 def display_lines(image, lines):
     line_image = np.zeros_like(image)
     if lines is not None:
@@ -52,19 +64,25 @@ def display_lines(image, lines):
             cv2.line(line_image, (x1,y1), (x2,y2), (0,255,0), 5)
     return line_image
 
+# Read in the image
 image = cv2.imread("test_image.jpg")
+# Copy it to another Numpy array
 lane_image = np.copy(image)
+# Detect edges
 canny_image = Canny(lane_image)
+# Identify the region of interest
 cropped_image = region_of_interest(canny_image)
+# Apply hough transform to identify lines
 lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength = 40, maxLineGap = 5)
+# Get a single left lane marker and right lane marker
 averaged_lines = average_lines(lane_image, lines)
+# Display the detected line
 line_image = display_lines(lane_image, averaged_lines)
+# Create a combination of original image and the detected lanes
 combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
-# cv2.imshow("Result", lane)
+
+# Save the detected file on disk
 cv2.imwrite("Detected_Lanes.jpg", combo_image)
-cv2.waitKey(0)
-
-
-# cap = cv2.VideoCapture("test2.mp4")
-# while(cap.isOpened()):
-#     _, frame = cap.read()
+# # Display the image
+# cv2.imshow("Result", combo_image)
+# cv2.waitKey(0)
--- lane_detection_video.py
+++ lane_detection_video.py
@@ -1,7 +1,13 @@
+# Shivam Chourey
+# Lane detection on a video
+# I learned this technique following tutorial by Rayan Smiles and PrgrammingKnowledge
+
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Function to get the coordinates where lines must be drawn
+# The lines are given by the argument line_parameters
 def make_coord(image, line_parameters):
     slope, intercept = line_parameters
     y1 = image.shape[0]
@@ -10,6 +16,8 @@ def make_coord(image, line_parameters):
     x2 = int((y2 - intercept)/slope)
     return np.array([x1, y1, x2, y2])
 
+# This function takes in a set of lines, identifies the left and right lines based on slope
+# Averages the left set and right set of lines to return a single left and a right line
 def average_lines(image, lines):
     left = []
     right = []
@@ -28,12 +36,15 @@ def average_lines(image, lines):
     right_line = make_coord(image, right_avg)
     return np.array([left_line, right_line])
 
-
+# This function takes in an image, converts it to grayscale and performs Canny edge detection
 def Canny(im):
     gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
     canny = cv2.Canny(gray, 100, 200)
     return canny
 
+# This function defines a region of interest in the image where we want to identify lanes
+# Right now, this has hard-coded values specific to the image and videos used
+# I plan to make it generic in near future
 def region_of_interest(im):
     height = im.shape[0]
     polygons = np.array([
@@ -44,6 +55,7 @@ def region_of_interest(im):
     masked_lane = np.bitwise_and(im, mask)
     return masked_lane
 
+# Function  to display lines on an image, both passed as argument
 def display_lines(image, lines):
     line_image = np.zeros_like(image)
     if lines is not None:
@@ -52,19 +64,31 @@ def display_lines(image, lines):
             cv2.line(line_image, (x1,y1), (x2,y2), (0,255,0), 5)
     return line_image
 
-
+# Main program
+# Capture the video
 cap = cv2.VideoCapture("test2.mp4")
 while(cap.isOpened()):
+    # Read the frame
     _, frame = cap.read()
+
+    # Detect edges
     canny_image = Canny(frame)
+    # Identify the region of interest
     cropped_image = region_of_interest(canny_image)
+    # Apply hough transform to identify lines
     lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength = 40, maxLineGap = 5)
+    # Get a single left lane marker and right lane marker
     averaged_lines = average_lines(frame, lines)
+    # Display the detected line
     line_image = display_lines(frame, averaged_lines)
+    # Create a combination of original image and the detected lanes
     combo_image = cv2.addWeighted(frame, 0.8, line_image, 1, 1)
-    # cv2.imshow("Result", lane)
+
+    # Display the detected lane for current frame
     cv2.imshow("Detected_Lanes", combo_image)
+    # Wait 1 ms or till q is pressed on keyboard
     if cv2.waitKey(1) == ord('q'):
         break
+# Release the video        
 cap.release()
 cv2.destroyAllWindows()
